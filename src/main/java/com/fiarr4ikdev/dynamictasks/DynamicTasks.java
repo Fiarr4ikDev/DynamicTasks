57d631bc610c63334b993d77950a96798326cf01
fiarr4ik <fiarr4ik@gmail.com>
2025-04-09 12:11:51 +0700

    Initial commit

---
+++ src/main/java/com/fiarr4ikdev/dynamictasks/DynamicTasks.java
@@ -0,0 +1,17 @@
+package com.fiarr4ikdev.dynamictasks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DynamicTasks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
